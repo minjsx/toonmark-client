fbb1c185216bcb8285dcaabce97501b7f0e9808c
minjsx <tyo2002@gmail.com>
2020-04-21 00:07:05 +0900

    Add ButtonPrimaryLight and Fix Buttons style

--- src/components/ui/Buttons.tsx
+++ src/components/ui/Buttons.tsx
@@ -2,21 +2,47 @@ import styled from 'styled-components';
 
 export const ButtonPrimary = styled('button')`
   display: flex;
-  width: 100%;
-  height: 100%;
+  /* width: 100%;
+  height: 100%; */
+  width: 7.5rem;
+  height: 2.5rem;
 
-  border-radius: 3px;
-  border-style: solid;
-  border-width: 1px;
-  border-color: #e3e3e3;
-  margin-bottom: 8px;
+  border-radius: 1rem;
+  border-width: 0px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 
   justify-content: center;
   align-items: center;
   cursor: pointer;
   background: ${(props): string => props.theme.btnPrimary};
   opacity: 1;
-  // transition: opacity 0.2s;
+  /* transition: opacity 0.2s; */
+
+  &:hover {
+    opacity: 0.5;
+  }
+  &:active {
+    opacity: 0.5;
+  }
+`;
+
+export const ButtonPrimaryLight = styled('button')`
+  display: flex;
+  /* width: 100%;
+  height: 100%; */
+  width: 7.5rem;
+  height: 2.5rem;
+
+  border-radius: 1rem;
+  border-width: 0px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background: ${(props): string => props.theme.btnPrimaryLight};
+  opacity: 1;
+  /* transition: opacity 0.2s; */
 
   &:hover {
     opacity: 0.5;
