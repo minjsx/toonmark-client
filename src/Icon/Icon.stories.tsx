bfff3727f9b321463992ae1777421e723bbc4c2f
minjsx <tyo2002@gmail.com>
2020-04-28 00:38:10 +0900

    Fix svg to SVGR Component

--- src/Icon/Icon.stories.tsx
+++ src/Icon/Icon.stories.tsx
@@ -1,25 +1,55 @@
+import Icon, { iconTypes } from './Icon';
 import React, { ReactElement } from 'react';
-import Icon from './Icon';
+import styled from 'styled-components';
 
 export default {
   component: Icon,
   title: 'components|Icon',
 };
 
-export const icon = (): ReactElement => <Icon icon="favorFill" />;
+const IconList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  li {
+    box-sizing: border-box;
+    width: 25%;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    svg {
+      margin-right: 1rem;
+    }
+  }
+`;
+
+export const icon = (): ReactElement => <Icon icon="FavorFill" />;
 
 icon.story = {
   name: 'Default',
 };
 
 export const customSize = (): ReactElement => (
-  <Icon icon="favorFill" size="4rem" />
+  <Icon icon="FavorFill" size="1rem" />
 );
 
 export const customColor = (): ReactElement => (
-  <Icon icon="favorFill" color="red" />
+  <Icon icon="FavorFill" color="red" />
 );
 
 export const customizedWithStyle = (): ReactElement => (
-  <Icon icon="favorFill" css={{ color: 'red', width: '4rem' }} />
+  <Icon icon="FavorFill" color="blue" size="4rem" />
 );
+
+export const listOfIcons = (): ReactElement => {
+  return (
+    <IconList>
+      {iconTypes.map((icon) => (
+        <li key={icon}>
+          <Icon icon={icon} />
+          {icon}
+        </li>
+      ))}
+    </IconList>
+  );
+};
