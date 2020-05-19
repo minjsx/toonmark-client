fceb442a4b2a0737164c8c01d0eaaed8d1db6426
minjsx <tyo2002@gmail.com>
2020-05-20 01:47:59 +0900

    Apply responsive style at WeekSelector

--- src/components/elements/WeekSelector.tsx
+++ src/components/elements/WeekSelector.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from 'react';
 import Button from './Button';
 import { Weekday } from '../../types';
 import styled from 'styled-components';
+import { useWIndowDemensions } from '../../hooks';
+import { size } from '../../theme';
 
 interface Props {
   selectedItem?: WeekDayType;
@@ -12,18 +14,19 @@ export type WeekDayType = 'ALL' | keyof typeof Weekday;
 type WeekDays = {
   key: WeekDayType;
   title: string;
+  shortTitle: string;
 };
 
 const weekdays: WeekDays[] = [
-  { key: 'ALL', title: '전체' },
-  { key: 'MON', title: '월요일' },
-  { key: 'TUE', title: '화요일' },
-  { key: 'WED', title: '수요일' },
-  { key: 'THU', title: '목요일' },
-  { key: 'FRI', title: '금요일' },
-  { key: 'SAT', title: '토요일' },
-  { key: 'SUN', title: '일요일' },
-  { key: 'TEN', title: '열흘' },
+  { key: 'ALL', title: '전체', shortTitle: '전체' },
+  { key: 'MON', title: '월요일', shortTitle: '월' },
+  { key: 'TUE', title: '화요일', shortTitle: '화' },
+  { key: 'WED', title: '수요일', shortTitle: '수' },
+  { key: 'THU', title: '목요일', shortTitle: '목' },
+  { key: 'FRI', title: '금요일', shortTitle: '금' },
+  { key: 'SAT', title: '토요일', shortTitle: '토' },
+  { key: 'SUN', title: '일요일', shortTitle: '일' },
+  { key: 'TEN', title: '열흘', shortTitle: '열흘' },
 ];
 
 const Templete = styled.section`
@@ -32,17 +35,21 @@ const Templete = styled.section`
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
-  width: 66.25rem;
-  height: 2.5rem;
+  width: 100%;
+  height: 100%;
+  max-height: 2.5rem;
+  overflow: none;
 `;
 
 export default function WeekSelector({ selectedItem }: Props): ReactElement {
+  const { width } = useWIndowDemensions();
+
   return (
     <Templete>
-      {weekdays.map(({ key, title }) => (
+      {weekdays.map(({ key, title, shortTitle }) => (
         <Button
           key={key}
-          text={title}
+          text={width > parseInt(size.laptop, 10) ? title : shortTitle}
           theme={key === selectedItem ? 'Primary' : 'PrimaryLight'}
         />
       ))}
