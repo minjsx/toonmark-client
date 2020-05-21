83272d9f4a84c0d80fb136777d3e46a83a4f12d6
minjsx <tyo2002@gmail.com>
2020-05-21 23:09:19 +0900

    Add Platform Screen

--- src/components/navigation/SwitchNavigator.tsx
+++ src/components/navigation/SwitchNavigator.tsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import React, { ReactElement } from 'react';
 
 import Intro from '../screen/Intro';
-import Temp from '../screen/Temp';
+import Platform from '../screen/Platform';
+// import Temp from '../screen/Temp';
 
 function SwitchNavigator(): ReactElement {
   return (
     <BrowserRouter>
       <div style={{ textAlign: 'center' }}>
         <Switch>
-          <Route exact={true} path="/" render={(): ReactElement => <Intro />} />
-          <Route render={(): ReactElement => <Temp />} />
+          <Route exact={true} path="/" component={Intro} />
+          <Route exact={true} path="/platform/:platform" component={Platform} />
+          {/* <Route component={Temp} /> */}
+          <Redirect path="*" to="/" />
         </Switch>
       </div>
     </BrowserRouter>
