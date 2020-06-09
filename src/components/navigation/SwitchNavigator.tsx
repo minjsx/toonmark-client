372c404e13b0fd7b00458b43b2b245ca5511d224
minjsx <tyo2002@gmail.com>
2020-06-09 23:39:14 +0900

    Apply to search link

--- src/components/navigation/SwitchNavigator.tsx
+++ src/components/navigation/SwitchNavigator.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import React, { ReactElement } from 'react';
 
 import Home from '../screen/Home';
+import Search from '../screen/Search';
 import Platform from '../screen/Platform';
 // import Temp from '../screen/Temp';
 
@@ -11,6 +12,7 @@ function SwitchNavigator(): ReactElement {
       <div style={{ textAlign: 'center' }}>
         <Switch>
           <Route exact={true} path="/" component={Home} />
+          <Route exact={true} path="/search" component={Search} />
           <Route exact={true} path="/platform/:platform" component={Platform} />
           {/* <Route component={Temp} /> */}
           <Redirect path="*" to="/" />
